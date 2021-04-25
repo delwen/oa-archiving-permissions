10c55a041b022f6ddd5e9820181e34c41c4db8dc
Delwen Franzen <delwen@franzen.fm>
2021-04-25 15:54:40 +0200

    Adapt for IntoValue dataset

--- scripts/02_get-open-access-status.R
+++ scripts/02_get-open-access-status.R
@@ -7,7 +7,7 @@ source(here::here("scripts", "environment.R"))
 # Define filepaths
 #-----------------------------------------------------------------------------------------------------------------------------
 
-filename <- "berlin-2018"
+filename <- "intovalue-filtered"
 
 input <- file.path(data_dir, paste0(filename, ".csv"))
 output <- file.path(data_dir, paste0(Sys.Date(), "_", filename, "-oa.csv"))
@@ -16,54 +16,38 @@ output <- file.path(data_dir, paste0(Sys.Date(), "_", filename, "-oa.csv"))
 # Read in dataset and get the OA status for all publications
 #-----------------------------------------------------------------------------------------------------------------------------
 
-doi_table <- read_csv(input, col_types = "ccdddcccccdccccdlllllc")
+doi_table <- read_csv(input, col_types = "cdcc")
 
 n_publications <- nrow(doi_table)
 
-cities <- doi_table$city %>% unique()
-years <- doi_table$year_published %>% unique()
-
-cols <- c("doi","city","year_published","color","issn","journal","publisher","date")
+cols <- c("doi","color","issn","journal","publisher","date")
 df <- data.frame(matrix(ncol = length(cols), nrow = 0))
 colnames(df) <- cols
 
-#Split dataset in chunks and do requests and save results on the chunks
-for(cty in cities)
-{
-  for(yr in years)
-  {
-    print(paste0("City: ", cty))
-    print(paste0("Year published: ", yr))
+doi_batch <- doi_table[["doi"]]
+print(paste0("DOI number: ", length(doi_batch)))
 
-    doi_batch <- (doi_table %>%
-                    filter(year_published == yr) %>%
-                    filter(city == cty))[["doi"]]
-    print(paste0("DOI number: ", length(doi_batch)))
+#retrieve OA colors for the DOIs from unpaywall
+unpaywall_results <- unpaywallR::dois_OA_colors(doi_batch,
+                                                email_api,
+                                                clusters = 2,
+                                                color_hierarchy = c("gold", "hybrid", "green", "bronze", "closed"))
 
-    #retrieve OA colors for the DOIs from unpaywall
-    unpaywall_results <- unpaywallR::dois_OA_colors(doi_batch,
-                                                    email_api,
-                                                    clusters = 2,
-                                                    color_hierarchy = c("gold", "hybrid", "green", "bronze", "closed"))
-    city_results <- tibble(city = cty,
-                           doi = doi_batch,
-                           color = unpaywall_results$OA_color,
-                           year_published = as.numeric(yr),
-                           issn = unpaywall_results$issn,
-                           journal = unpaywall_results$journal,
-                           publisher = unpaywall_results$publisher,
-                           date = unpaywall_results$date)
+oa_results <- tibble(doi = doi_batch,
+                       color = unpaywall_results$OA_color,
+                       issn = unpaywall_results$issn,
+                       journal = unpaywall_results$journal,
+                       publisher = unpaywall_results$publisher,
+                       date = unpaywall_results$date)
 
-    df <- rbind(df, city_results)
-  }
-}
+df <- rbind(df, oa_results)
 
 # all_results <- merge(doi_table, df)
 # all_results <- all_results %>%
 #   distinct(city, year_published, doi, .keep_all = TRUE)
 # all_results <- all_results[order(all_results$city),]
 
-all_results <- left_join(doi_table, df, by = c("doi", "city", "year_published"))
+all_results <- left_join(doi_table, df, by = c("doi"))
 
 write_csv(all_results, output)
 
