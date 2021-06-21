dc82bc2fd74cd5c0fbcbb12680c93b29943cead5
Delwen Franzen <delwen@franzen.fm>
2021-06-21 15:22:45 +0200

    Update plot of OA status

--- scripts/06_plot-oa-data.R
+++ scripts/06_plot-oa-data.R
@@ -11,17 +11,18 @@ OA_data <- data %>%
   mutate(perc = (count/sum(OA_data$count))*100) %>%
   arrange(desc(perc)) %>%
   mutate(lab_ypos = cumsum(perc) - 0.5 * perc) %>%
-  mutate(publications = "IntoValue")
+  mutate(publications = "")
 
 OA_data$color <- factor(OA_data$color, levels = OA_data$color[6:1])
 
 png(file.path(data_dir, paste0(filename, "-status.png")),
-    width = 6,
+    width = 5,
     height = 4,
     units = 'in',
     res = 300)
 
-first <- "#E2D7BF"
+#first <- "#E2D7BF"
+first <- "#ccbe9f"
 second <- "#DDB247"
 third <- "#C3675D"
 fourth <- "#457373"
@@ -29,10 +30,10 @@ fifth <- "#5B3547"
 sixth <- "#131826"
 
 s <- ggplot(data = OA_data, aes(x = publications, y = perc)) + 
-  geom_col(aes(fill = color), width = 0.7) +
+  geom_col(aes(fill = color), width = 0.5, alpha = 0.7) +
   ggtitle(paste0("OA status of clinical trial publications (n = ", sum(OA_data$count), ")")) +
   geom_text(aes(y = lab_ypos, label = round(perc, digits = 1), group=color), color = "white") +
-  ylab("Percentage publications (%)") + 
+  ylab("Percentage publications (%)") + xlab("") +  
   scale_fill_manual(values = c(sixth, fifth, fourth, third, second, first))
 
 print(s)
