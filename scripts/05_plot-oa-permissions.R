bc5f845a22fb0c050b5054cbf7448b0eff45b3c5
Delwen Franzen <delwen@franzen.fm>
2021-06-21 15:20:02 +0200

    Update plot for OA permissions

--- scripts/05_plot-oa-permissions.R
+++ scripts/05_plot-oa-permissions.R
@@ -8,28 +8,28 @@ results <- data %>%
   filter(color == "closed") %>%
   rename(permission = permission_postprint) %>%
   mutate(permission = ifelse(permission, "yes", "no")) %>%
-  mutate(permission = ifelse(is.na(permission), "nodata", permission)) %>%
+  mutate(permission = ifelse(is.na(permission), "no data", permission)) %>%
   count(permission) %>%
   rename(count = n) %>%
   arrange(desc(count)) %>%
   mutate(lab_ypos = cumsum(count) - 0.5 * count) %>%
-  mutate(publications = "closed")
+  mutate(publications = "")
 
 png(file.path(data_dir, paste0(filename, ".png")),
-    width = 6,
+    width = 5,
     height = 4,
     units = 'in',
     res = 300)
 
-first <- "#1D9A6C"
+first <- "#56B870"
 second <- "#137177"
 third <- "#0A2F51"
 
 p <- ggplot(data = results, aes(x = publications, y = count)) + 
-  geom_col(aes(fill = permission), width = 0.7) +
-  ggtitle(paste0("Permission to archive the postprint for closed publications (n = ", sum(results$count), ")")) +
+  geom_col(aes(fill = permission), width = 0.5, alpha = 0.8) +
+  ggtitle("Permission to archive the postprint") +
   geom_text(aes(y = lab_ypos, label = round(count, digits = 1), group=permission), color = "white") +
-  ylab("Publications") + 
+  ylab("Closed publications") + xlab("") +
   scale_fill_manual(values = c(third, second, first))
 
 print(p)
