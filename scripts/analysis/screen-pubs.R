4174da82a4d546fabc6d9616143f22a3a4a3e8d3
Delwen Franzen <delwen@franzen.fm>
2022-08-04 19:46:47 +0200

    Prepare and generate publication screening flowchart
    - Inclusion/exclusion criteria for trial publications
    - Adapted from Maia Salholz-Hillel

---
+++ scripts/analysis/screen-pubs.R
@@ -0,0 +1,105 @@
+#TODO: find a way to move dupes screening criteria to after DOI criterion
+
+library(dplyr)
+library(readr)
+library(tidyr)
+library(stringr)
+library(here)
+
+# Prepare trial publications ----------------------------------------------------------
+
+intovalue_pubs_oa <- read_csv(here("data", "intovalue_pubs_oa.csv"))
+
+pubs_raw <- intovalue_pubs_oa %>%
+  mutate(
+    has_doi = if_else(!is.na(doi), TRUE, FALSE),
+    is_resolved_unpaywall = if_else(!is.na(doi) & !is.na(color), TRUE, FALSE),
+    is_published_2010_2020 = if_else(publication_year_unpaywall > "2009" & publication_year_unpaywall < "2021", TRUE, FALSE)
+  )
+
+# Prepare screening functions ---------------------------------------------
+
+# Apply screening criteria
+# Returns list of inclusion counts and filtered dataframe
+count_filter <- function(data, vars) {
+  
+  counts <-
+    tibble(name = as.character(),
+           value = as.logical(),
+           n = as.integer()
+    )
+  
+  for (var in vars) {
+    counts <-
+      data %>%
+      count(.data[[var]]) %>%
+      pivot_longer(-n) %>%
+      add_row(counts, .)
+    
+    data <- filter(data, .data[[var]])
+  }
+  
+  list(data = data, counts = counts)
+  
+}
+
+# Report screening summary counts
+report_n <- function(counts, var, condition) {
+  n <-
+    counts %>%
+    filter(name == var & value == condition) %>%
+    pull(n)
+  
+  # If empty, count is 0
+  if (rlang::is_empty(n)){n <- 0}
+  
+  n
+}
+
+
+# Screen trial publications -----------------------------------------------------------
+
+screening_criteria <- c(
+  "has_doi",
+  "is_resolved_unpaywall",
+  "is_published_2010_2020"
+)
+
+pubs_screened <- count_filter(pubs_raw, screening_criteria)
+
+pubs <- pubs_screened$data
+
+#write_csv(pubs, here("data", "pubs.csv"))
+
+# Tabularize pub screening counts ---------------------------------------
+
+pubs_screening <-
+  
+  pubs_raw %>%
+
+  bind_rows(pubs_screened$counts) %>% 
+  
+  add_row(
+   name = "is_unique_doi",
+   value = c(TRUE, FALSE),
+   n = c(n_distinct(pubs$doi),
+         nrow(pubs) - n_distinct(pubs$doi))
+ )
+
+
+# Report trial screening counts -------------------------------------------
+
+n_pubs_iv <- nrow(pubs_raw)
+n_pubs_doi <- report_n(pubs_screening, "has_doi", TRUE)
+n_pubs_doi_ex <- report_n(pubs_screening, "has_doi", FALSE)
+n_pubs_resolved <- report_n(pubs_screening, "is_resolved_unpaywall", TRUE)
+n_pubs_resolved_ex <- report_n(pubs_screening, "is_resolved_unpaywall", FALSE)
+n_pubs_2010_2020 <- report_n(pubs_screening, "is_published_2010_2020", TRUE)
+n_pubs_2010_2020_ex <- report_n(pubs_screening, "is_published_2010_2020", FALSE)
+n_pubs_deduped <- report_n(pubs_screening, "is_unique_doi", TRUE)
+n_pubs_deduped_ex <- report_n(pubs_screening, "is_unique_doi", FALSE)
+
+
+# Remove unnecessary variables --------------------------------------------
+
+rm(screening_criteria, pubs_screened, report_n)
