82aeccbd0201d9b7e63f3f3e85a30b98eaefd43d
Delwen Franzen <delwen@franzen.fm>
2022-12-16 17:18:54 +0100

    Aesthetic changes to plots

--- scripts/generate-plots.R
+++ scripts/generate-plots.R
@@ -41,7 +41,7 @@ archiving_plot$status <- factor(archiving_plot$status, levels = c("no_data",
                                                     "archived"
                                                     ))
 
-levels(archiving_plot$status) <- list("Unclear" = "no_data",
+levels(archiving_plot$status) <- list("Permission unclear" = "no_data",
                               "Permission not found" = "permission_not_found",
                               "Permission found" = "permission_found",
                               "Archived" = "archived"
@@ -53,16 +53,21 @@ archiving_plot$publication_year_unpaywall <- factor(archiving_plot$publication_y
 t <- ggplot(archiving_plot, aes(fill=status, y=n, x=publication_year_unpaywall)) + 
   geom_bar(position="stack", stat="identity", color="black") +
   scale_x_discrete(breaks = seq(from = 2010, to = 2020, by = 2)) +
-  scale_fill_manual(values=c('#20303b', '#ab880c', '#539d66', '#007265'), name=NULL) +
+  scale_fill_manual(values=c('#20303b', '#ab880c', '#539d66', '#00584e'), name = NULL) +
   xlab("Year of publication") +
-  ylab("Closed-access publications") +
+  ylab("Paywalled publications") +
   ylim(0,160) +
   theme_classic() +
   theme(panel.grid.major.y = element_line(color = "black",
                                     size = 0.3,
-                                    linetype = 3)) +
-  theme(axis.text = element_text(size = 10)) +
-  theme(axis.title = element_text(size = 12, face = "bold"))
+                                    linetype = 3),
+        axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14, face = "bold"),
+        axis.title.x = element_text(margin = margin(t = 15, r = 0, b = 0, l = 0)),
+        axis.title.y = element_text(margin = margin(t = 0, r = 15, b = 0, l = 0)),
+        legend.text = element_text(size=13),
+        legend.spacing.y = unit(0.1, 'cm')) +
+  guides(fill = guide_legend(byrow = TRUE))
 
 ggsave(here("figures", "archiving-status.tiff"), t, height = 5, width = 7, dpi = 320)
 
@@ -94,10 +99,15 @@ p <- ggplot(oa_plot, aes(fill=color, y=n, x=publication_year_unpaywall)) +
   scale_y_continuous(labels = scales::percent_format()) +
   scale_fill_manual(values=c('#B6B6B6', '#007265', '#cf9188', '#634587', '#F1BA50'), name=NULL) +
   xlab("Year of publication") +
-  ylab("Percentage publications (%)") +
+  ylab("Percentage publications") +
   theme_classic() +
-  theme(axis.text=element_text(size=10)) +
-  theme(axis.title = element_text(size = 12, face = "bold"))
+  theme(axis.text=element_text(size=14),
+        axis.title = element_text(size = 14, face = "bold"),
+        axis.title.x = element_text(margin = margin(t = 15, r = 0, b = 0, l = 0)),
+        axis.title.y = element_text(margin = margin(t = 0, r = 15, b = 0, l = 0)),
+        legend.text = element_text(size=13),
+        legend.spacing.y = unit(0.1, 'cm')) + 
+  guides(fill = guide_legend(byrow = TRUE))
 
 ggsave(here("figures", "oa-status.tiff"), p, height = 5, width = 7, dpi = 320)
 
@@ -117,15 +127,17 @@ embargo_distribution <- data %>%
 e <- ggplot(
   embargo_distribution,
   aes(x = embargo, y=number)) + 
-  geom_bar(stat = "identity") +
-  geom_text(aes(label = number), color = "black", size = 3.5, vjust = -0.5) +
+  geom_bar(stat = "identity", fill = '#9fb6cd') +
+  geom_text(aes(label = number), color = "black", size = 4.5, vjust = -0.5) +
   ylim(0,800) +
   labs(y= "Paywalled publications", x = "Embargo length (months)") +
   theme_classic() +
   theme(panel.grid.major.y = element_line(color = "black",
                                           size = 0.1,
-                                          linetype = 3)) +
-  theme(axis.text = element_text(size = 10)) +
-  theme(axis.title = element_text(size = 12, face = "bold"))
+                                          linetype = 3),
+        axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14, face = "bold"),
+        axis.title.x = element_text(margin = margin(t = 15, r = 0, b = 0, l = 0)),
+        axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0)))
 
 ggsave(here("figures", "embargo.tiff"), e, height = 5, width = 7, dpi = 320)
